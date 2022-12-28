72e5ec5a799a0df87344e515c824b0fbeab4ac73
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2022-12-28 18:42:21 +0500

    Added redirection on successful login.

--- src/app/authenticate-user.service.ts
+++ src/app/authenticate-user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginUsersListService } from './login-users-list.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateUserService {
+  
 
-  constructor() { }
+  constructor(
+    private currentUser: LoginUsersListService,
+    private route:Router
+    ) { }
+
+  onLoginUser(name:string, password:string) {
+    if (name == this.currentUser.loginUsers[0].userName &&
+      password == this.currentUser.loginUsers[0].password) {
+      this.route.navigate(['/'])
+    }
+    else{
+      this.route.navigate(['/Login'])
+    }
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { LoginUsersListService } from '../login-users-list.service';
+import { AuthenticateUserService } from '../authenticate-user.service';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +8,19 @@ import { LoginUsersListService } from '../login-users-list.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-constructor(private currentUser:LoginUsersListService){
+constructor(private checkUser:AuthenticateUserService){
 
 }
 
 loginForm: FormGroup = new FormGroup({
   name: new FormControl('', [Validators.required]),
-  password: new FormControl('', [Validators.required, Validators.minLength(2)]),
+  password: new FormControl('', [Validators.required, Validators.minLength(3)]),
 })
 
-onLoginUser(){
-  if (this.loginForm.value.name == this.currentUser.loginUsers[0].userName && 
-    this.loginForm.value.password == this.currentUser.loginUsers[0].password){
-  } 
+onValidateLogin(){
+  let checkUserName = this.loginForm.value.name;
+  let checkUserPassword = this.loginForm.value.password;
+
+  this.checkUser.onLoginUser(checkUserName, checkUserPassword);
 }
 }
